85f7e82b312347fe2e8ba3d55c4e02f9b6e519cf
preugen4-code <preugen4@gmail.com>
2026-08-04 23:36:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+require("dotenv").config();
 
+const {
+  Client,
+  GatewayIntentBits,
+  Events
+} = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once(Events.ClientReady, () => {
+  console.log(`✅ Eingeloggt als ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
