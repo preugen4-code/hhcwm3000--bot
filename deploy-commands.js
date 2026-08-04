6a28f5a134ae39136799c00e28d70b49012d0769
preugen4-code <preugen4@gmail.com>
2026-08-04 23:46:18 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,30 @@
+require("dotenv").config();
 
+const { REST, Routes } = require("discord.js");
+
+const commands = [
+    {
+        name: "ping",
+        description: "Zeigt die Bot-Latenz an."
+    }
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+    try {
+        console.log("Registriere Slash-Commands...");
+
+        await rest.put(
+            Routes.applicationGuildCommands(
+                "1534310295378329751",
+                "1457819772455751863"
+            ),
+            { body: commands }
+        );
+
+        console.log("✅ Slash-Commands registriert!");
+    } catch (error) {
+        console.error(error);
+    }
+})();
