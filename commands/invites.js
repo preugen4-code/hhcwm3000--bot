248e1403794798d72564481be9f5df4b0573c24b
preugen4-code <preugen4@gmail.com>
2026-08-04 23:48:55 +0200

    Create invites.js

---
+++ commands/invites.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require("discord.js");
+const db = require("../database/db");
+const { getOrCreateInvite } = require("../utils/inviteManager");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("invites")
+        .setDescription("Zeigt deinen Invite-Link und deine Invites."),
+
+    async execute(interaction) {
+
+        const code = await getOrCreateInvite(interaction.member);
+
+        let user = db.prepare(
+            "SELECT * FROM users WHERE id = ?"
+        ).get(interaction.user.id);
+
+        if (!user) {
+            db.prepare(
+                "INSERT INTO users (id, inviteCode, invites) VALUES (?, ?, 0)"
+            ).run(interaction.user.id, code);
+
+            user = {
+                inviteCode: code,
+                invites: 0
+            };
+        }
+
+        await interaction.reply({
+            embeds: [{
+                title: "🎉 Deine Invites",
+                description:
+`**Invite Link**
+https://discord.gg/${user.inviteCode}
+
+**Invites**
+${user.invites}/5`,
+                color: 0x8b5cf6
+            }],
+            ephemeral: true
+        });
+
+    }
+};
