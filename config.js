cb7a45c86c8c2df68f6265ac80a5ea35460ea6c1
preugen4-code <preugen4@gmail.com>
2026-08-04 23:48:06 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    guildId: "1457819772455751863",
+    applicationId: "1534310295378329751",
+
+    inviteRequirement: 5,
+
+    giveawayColor: "#8b5cf6",
+
+    inviteChannelName: "inv-tracker"
+};
